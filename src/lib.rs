57bf88ec280fbc91fc24cae0e802bda4a8f1cdcd
Dániel Buga <bugadani@gmail.com>
2020-05-07 09:45:40 +0200

    Safer wrapping addition for indices

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,66 @@ pub trait Reader {
     fn iter_unordered(&self) -> UnorderedIter<Self::Item, Self::WindowSize>;
 }
 
+mod wrapping {
+    pub trait WrappingExt {
+        type Rhs;
+        type Output;
+        fn wrapping_add_limited(self, r: Self::Rhs, max: Self::Rhs) -> Self::Output;
+        fn wrapping_add1_limited(self, max: Self::Rhs) -> Self::Output;
+    }
+
+    impl WrappingExt for usize {
+        type Rhs = Self;
+        type Output = Self;
+        fn wrapping_add_limited(self, r: Self::Rhs, max: Self::Rhs) -> Self::Output {
+            match self.checked_add(r) {
+                Some(v) => v % max,
+                None => (r - (usize::MAX - self)) % max
+            }
+        }
+
+        fn wrapping_add1_limited(self, max: Self::Rhs) -> Self::Output {
+            if self == max - 1 { 0 } else { self + 1 }
+        }
+    }
+
+    #[cfg(test)]
+    mod test {
+        use super::WrappingExt;
+
+        #[test]
+        pub fn sanity_check() {
+            let vector: &[(usize, usize, usize, usize)] = &[
+                (5, 1, 10, 6),
+                (5, 5, 10, 0),
+                (5, 6, 10, 1),
+                (5, 16, 10, 1),
+                (usize::MAX, usize::MAX, usize::MAX, 0),
+                (usize::MAX, 1, usize::MAX, 1),
+                (usize::MAX - 1, 2, usize::MAX, 1)
+            ];
+
+            for &(lhs, rhs, limit, expectation) in vector.iter() {
+                assert_eq!(expectation, lhs.wrapping_add_limited(rhs, limit), "({} + {}) mod {} == {}", lhs, rhs, limit, expectation);
+            }
+        }
+
+        #[test]
+        pub fn sanity_check_increment() {
+            let vector: &[(usize, usize, usize)] = &[
+                (5, 10, 6),
+                (9, 10, 0)
+            ];
+
+            for &(lhs, limit, expectation) in vector.iter() {
+                assert_eq!(lhs.wrapping_add_limited(1, limit), lhs.wrapping_add1_limited(limit), "({} + 1) mod {} == {}", lhs, limit, expectation);
+            }
+        }
+    }
+}
+
 use generic_array::{GenericArray, ArrayLength};
+use wrapping::WrappingExt as _;
 pub trait Size<I>: ArrayLength<Option<I>> {}
 impl<T, I> Size<I> for T where T: ArrayLength<Option<I>> {}
 
@@ -38,7 +97,7 @@ impl<IT, N> core::ops::Index<usize> for SlidingWindow<IT, N>
     type Output = IT;
     fn index(&self, idx: usize) -> &Self::Output {
         let read_from = if self.full() {
-            (self.write_idx + idx) % N::to_usize()
+            self.write_idx.wrapping_add_limited(idx, N::to_usize())
         } else {
             idx % N::to_usize()
         };
@@ -61,7 +120,7 @@ impl<'a, IT, N> Iterator for Iter<'a, IT, N>
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.offset < self.count {
-            let read_from = (self.start + self.offset) % N::to_usize();
+            let read_from = self.start.wrapping_add_limited(self.offset, N::to_usize());
             self.offset += 1;
 
             self.window.items[read_from].as_ref()
@@ -110,11 +169,7 @@ impl<IT, N> Producer for SlidingWindow<IT, N>
 
     fn insert(&mut self, t: Self::Item) -> Option<Self::Item> {
         let old = self.items[self.write_idx].replace(t);
-        if self.write_idx == N::to_usize() - 1 {
-            self.write_idx = 0;
-        } else {
-            self.write_idx += 1;
-        }
+        self.write_idx = self.write_idx.wrapping_add1_limited(N::to_usize());
 
         old
     }
