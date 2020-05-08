95634842c514f64fadf518544d5bf5ad39c72be8
Dániel Buga <bugadani@gmail.com>
2020-05-08 10:24:54 +0200

    General cleanup, add doc comments, fill in metadata

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 SlidingWindow
 =============
 
-SlidingWindow is a fixed size circular buffer that, when written, overwrites the oldest element.
+Sliding windows are used to hold the N most recent samples of a data stream.
+
+Example
+-------
+
+```rust
+use sliding_window::*;
+use sliding_window::typenum::consts::*;
+
+// Create a SlidingWindow with a window size of 4 elements
+let mut sw: SlidingWindow<_, U4> = SlidingWindow::new();
+
+// Insert some data
+sw.insert(1);
+sw.insert(2);
+sw.insert(3);
+sw.insert(4);
+
+// The 0 index always returns the oldest element in the window
+assert_eq!(1, sw[0]);
+
+// When full, inserting a new element removes and returns the oldest
+assert_eq!(Some(1), sw.insert(5));
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,31 @@
-//! This crate provides a heapless sliding window. Sliding windows are used to hold the last N samples of a data stream.
+//! This crate provides a heapless, fixed size sliding window.
 //!
+//! Sliding windows are used to hold the N most recent samples of a data stream.
+//!
+//! # Example
+//!
+//! ```rust
+//! use sliding_window::*;
+//! use sliding_window::typenum::consts::*;
+//!
+//! // Create a SlidingWindow with a window size of 4 elements
+//! let mut sw: SlidingWindow<_, U4> = SlidingWindow::new();
+//!
+//! // Insert some data
+//! sw.insert(1);
+//! sw.insert(2);
+//! sw.insert(3);
+//! sw.insert(4);
+//!
+//! // The 0 index always returns the oldest element in the window
+//! assert_eq!(1, sw[0]);
+//!
+//! // When full, inserting a new element removes and returns the oldest
+//! assert_eq!(Some(1), sw.insert(5));
+//! ```
 #![cfg_attr(not(test), no_std)]
 
 pub use generic_array::typenum;
-pub use generic_array::typenum::consts;
-
-pub trait Producer {
-    type Item;
-
-    fn clear(&mut self);
-    fn insert(&mut self, t: Self::Item) -> Option<Self::Item>;
-}
-
-pub trait Reader {
-    type Item;
-    type WindowSize: Size<Self::Item>;
-
-    fn full(&self) -> bool;
-    fn count(&self) -> usize;
-    fn iter(&self) -> Iter<Self::Item, Self::WindowSize>;
-    fn iter_unordered(&self) -> UnorderedIter<Self::Item, Self::WindowSize>;
-}
 
 mod wrapping {
     pub trait WrappingExt {
@@ -82,11 +87,17 @@ mod wrapping {
 
 use generic_array::{GenericArray, ArrayLength};
 use wrapping::WrappingExt as _;
+
 pub trait Size<I>: ArrayLength<Option<I>> {}
 impl<T, I> Size<I> for T where T: ArrayLength<Option<I>> {}
 
+/// A sliding window.
+///
+/// Sliding windows are queues that overwrite their oldest data when full.
+#[derive(Default)]
 pub struct SlidingWindow<IT, N>
-    where N: Size<IT> {
+    where
+        N: Size<IT> {
     items: GenericArray<Option<IT>, N>,
     write_idx: usize
 }
@@ -96,7 +107,7 @@ impl<IT, N> core::ops::Index<usize> for SlidingWindow<IT, N>
         N: Size<IT> {
     type Output = IT;
     fn index(&self, idx: usize) -> &Self::Output {
-        let read_from = if self.full() {
+        let read_from = if self.is_full() {
             self.write_idx.wrapping_add_limited(idx, N::to_usize())
         } else {
             idx % N::to_usize()
@@ -106,8 +117,10 @@ impl<IT, N> core::ops::Index<usize> for SlidingWindow<IT, N>
     }
 }
 
+/// Read-only iterator that returns elements in the order of insertion.
 pub struct Iter<'a, IT, N>
-    where N: Size<IT> {
+    where
+        N: Size<IT> {
     window: &'a SlidingWindow<IT, N>,
     start: usize,
     offset: usize,
@@ -115,7 +128,8 @@ pub struct Iter<'a, IT, N>
 }
 
 impl<'a, IT, N> Iterator for Iter<'a, IT, N>
-    where N: Size<IT> {
+    where N:
+        Size<IT> {
     type Item = &'a IT;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -130,14 +144,17 @@ impl<'a, IT, N> Iterator for Iter<'a, IT, N>
     }
 }
 
+/// Read-only iterator that does not respect the order of insertion.
 pub struct UnorderedIter<'a, IT, N>
-    where N: Size<IT> {
+    where
+        N: Size<IT> {
     window: &'a SlidingWindow<IT, N>,
     offset: usize
 }
 
 impl<'a, IT, N> Iterator for UnorderedIter<'a, IT, N>
-    where N: Size<IT> {
+    where
+        N: Size<IT> {
     type Item = &'a IT;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -152,64 +169,66 @@ impl<'a, IT, N> Iterator for UnorderedIter<'a, IT, N>
 }
 
 impl<IT, N> SlidingWindow<IT, N>
-    where N: Size<IT> {
+    where
+        N: Size<IT> {
+
+    /// Returns an empty sliding window object.
     pub fn new() -> Self {
         Self {
             items: GenericArray::default(),
             write_idx: 0
         }
     }
-}
 
-impl<IT, N> Producer for SlidingWindow<IT, N>
-    where N: Size<IT> {
-    type Item = IT;
-
-    fn insert(&mut self, t: Self::Item) -> Option<Self::Item> {
+    /// Insert an element into the window.
+    ///
+    /// If the window is full, this method will remove and return the oldest element.
+    pub fn insert(&mut self, t: IT) -> Option<IT> {
         let old = self.items[self.write_idx].replace(t);
         self.write_idx = self.write_idx.wrapping_add1_limited(N::to_usize());
 
         old
     }
 
-    fn clear(&mut self) {
-        self.write_idx = 0;
-        for i in 0..N::to_usize() {
-            self.items[i] = None;
-        }
+    /// Removes all elements from the window.
+    pub fn clear(&mut self) {
+        *self = Self::new();
     }
-}
 
-impl<IT, N> Reader for SlidingWindow<IT, N>
-    where N: Size<IT> {
-    type Item = IT;
-    type WindowSize = N;
-
-    fn full(&self) -> bool {
+    /// Returns `true` if the window is full.
+    pub fn is_full(&self) -> bool {
         match self.items[self.write_idx] {
             Some(_) => true,
             None => false
         }
     }
 
-    fn count(&self) -> usize {
-        if self.full() {
+    /// Returns the number of elements stored in the window.
+    pub fn count(&self) -> usize {
+        if self.is_full() {
             N::to_usize()
         } else {
             self.write_idx
         }
     }
 
-    fn iter(&self) -> Iter<Self::Item, Self::WindowSize> {
+    /// Returns an iterator to read from the window.
+    ///
+    /// The iterator starts at the oldest element and ends with the newest.
+    pub fn iter(&self) -> Iter<IT, N> {
         Iter {
             window: self,
-            start: if self.full() { self.write_idx } else { 0 },
+            start: if self.is_full() { self.write_idx } else { 0 },
             offset: 0,
             count: self.count()
         }
     }
 
-    fn iter_unordered(&self) -> UnorderedIter<Self::Item, Self::WindowSize> {
+    /// Returns an iterator to read from the window.
+    ///
+    /// This iterator starts at the beginning of the internal array instead of the oldest element
+    /// so it does not return the elements in the order of insertion.
+    pub fn iter_unordered(&self) -> UnorderedIter<IT, N> {
         UnorderedIter {
             window: self,
             offset: self.count()
@@ -220,7 +239,7 @@ impl<IT, N> Reader for SlidingWindow<IT, N>
 #[cfg(test)]
 mod test {
     use super::*;
-    use super::consts::*;
+    use super::typenum::consts::*;
 
     #[test]
     fn basics() {
@@ -233,24 +252,24 @@ mod test {
         assert_eq!(1, sw[0]);
 
         assert_eq!(3, sw.count());
-        assert_eq!(false, sw.full());
+        assert_eq!(false, sw.is_full());
 
         assert_eq!(None, sw.insert(4));
 
         assert_eq!(1, sw[0]);
         assert_eq!(4, sw.count());
-        assert_eq!(true, sw.full());
+        assert_eq!(true, sw.is_full());
 
         assert_eq!(Some(1), sw.insert(5));
 
         assert_eq!(2, sw[0]);
         assert_eq!(4, sw.count());
-        assert_eq!(true, sw.full());
+        assert_eq!(true, sw.is_full());
 
         sw.clear();
 
         assert_eq!(0, sw.count());
-        assert_eq!(false, sw.full());
+        assert_eq!(false, sw.is_full());
     }
 
     #[test]
